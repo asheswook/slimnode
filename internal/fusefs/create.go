b8b2584c965335fea60df1c8da5ea751b8b39c14
Jaewook Lee <a@pororo.ro>
2026-04-19 01:42:13 +0900

    feat(fusefs): return NullWriteHandle for non-ACTIVE file creation

--- internal/fusefs/create.go
+++ internal/fusefs/create.go
@@ -2,6 +2,7 @@ package fusefs
 
 import (
 	"context"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"syscall"
@@ -16,6 +17,25 @@ import (
 var _ fs.NodeCreater = (*RootNode)(nil)
 
 func (r *RootNode) Create(ctx context.Context, name string, flags uint32, mode uint32, out *fuse.EntryOut) (*fs.Inode, fs.FileHandle, uint32, syscall.Errno) {
+	if existing, _ := r.fs.st.GetFile(name); existing != nil {
+		switch existing.State {
+		case store.FileStateRemote, store.FileStateCached, store.FileStateLocalFinalized:
+			slog.Warn("Create called on non-ACTIVE file, returning NullWriteHandle",
+				"file", name, "state", existing.State)
+			out.Attr.Mode = fuse.S_IFREG | 0644
+			out.Attr.Size = uint64(existing.Size)
+			out.SetAttrTimeout(attrTimeoutShort)
+			out.SetEntryTimeout(entryTimeoutShort)
+
+			node := &FileNode{fs: r.fs, filename: name}
+			stable := fs.StableAttr{Ino: InodeForFile(name), Mode: fuse.S_IFREG}
+			inode := r.NewInode(ctx, node, stable)
+
+			nh := &NullWriteHandle{fs: r.fs, filename: name}
+			return inode, nh, fuse.FOPEN_KEEP_CACHE, fs.OK
+		}
+	}
+
 	path := filepath.Join(r.fs.localDir, name)
 	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|int(flags)&^os.O_EXCL, os.FileMode(mode))
 	if err != nil {
